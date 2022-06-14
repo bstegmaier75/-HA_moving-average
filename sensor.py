d1eeef7b0d6bb50c37db6e2216e44665558da3ce
Bernhard Stegmaier <stegmaier@sw-systems.de>
2022-06-14 19:04:16 +0200

    Implementaton of moving window average triggered by entity updates.

--- sensor.py
+++ sensor.py
@@ -4,7 +4,12 @@ from __future__ import annotations
 import logging
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
-from collections import deque 
+from collections import deque
+from datetime import (
+    datetime, 
+    timedelta
+)
+from typing import Tuple
 
 from . import (
     DOMAIN, 
@@ -75,7 +80,7 @@ async def async_setup_platform(
     name = config.get(CONF_NAME)
     unique_id = config.get(CONF_UNIQUE_ID)
     entity_id = config.get(CONF_ENTITY_ID)
-    avg = MovingAvg(config.get(CONF_FILTER_WINDOW_SIZE), config.get(CONF_FILTER_PRECISION))
+    avg = MovingAvg(name, config.get(CONF_FILTER_WINDOW_SIZE), config.get(CONF_FILTER_PRECISION))
 
     async_add_entities([SensorMovingAvg(name, unique_id, entity_id, avg)])
 
@@ -115,19 +120,24 @@ class SensorMovingAvg(SensorEntity):
             self.async_write_ha_state()
             return
 
-        val = float(new_state.state)
-        self._state = self._avg.update_value(val, new_state.last_updated)
+        if new_state.last_changed == new_state.last_updated:
+            # get attributes from entitity
+            if self._icon is None:
+                self._icon = new_state.attributes.get(ATTR_ICON, DEFAULT_ICON)
+            if (self._device_class is None) and (new_state.attributes.get(ATTR_DEVICE_CLASS) in SENSOR_DEVICE_CLASSES):
+                self._device_class = new_state.attributes.get(ATTR_DEVICE_CLASS)
+            if (self._attr_state_class is None) and (new_state.attributes.get(SENSOR_ATTR_STATE_CLASS) in SENSOR_STATE_CLASSES):
+                self._attr_state_class = new_state.attributes.get(SENSOR_ATTR_STATE_CLASS)
+            if self._unit_of_measurement is None:
+                self._unit_of_measurement = new_state.attributes.get(ATTR_UNIT_OF_MEASUREMENT)
+            # update moving average
+            new_val = float(new_state.state)
+            _LOGGER.debug(f"{self._name}: Updating with {new_val}")
+            self._state = self._avg.update_value(new_val, new_state.last_changed)
+            self.async_write_ha_state()
+        else:
+            _LOGGER.debug(f"{self._name}: Not updating, last_changed != last_updated")
 
-        if self._icon is None:
-            self._icon = new_state.attributes.get(ATTR_ICON, DEFAULT_ICON)
-        if (self._device_class is None) and (new_state.attributes.get(ATTR_DEVICE_CLASS) in SENSOR_DEVICE_CLASSES):
-            self._device_class = new_state.attributes.get(ATTR_DEVICE_CLASS)
-        if (self._attr_state_class is None) and (new_state.attributes.get(SENSOR_ATTR_STATE_CLASS) in SENSOR_STATE_CLASSES):
-            self._attr_state_class = new_state.attributes.get(SENSOR_ATTR_STATE_CLASS)
-        if self._unit_of_measurement is None:
-            self._unit_of_measurement = new_state.attributes.get(ATTR_UNIT_OF_MEASUREMENT)
-
-        self.async_write_ha_state()
 
     async def async_added_to_hass(self):
         """Register callbacks."""
@@ -165,7 +175,7 @@ class SensorMovingAvg(SensorEntity):
     @property
     def extra_state_attributes(self):
         """Return the state attributes of the sensor."""
-        return {ATTR_ENTITY_ID: self._entity}
+        return {ATTR_ENTITY_ID: self._entity, "data_points": self._avg.data_points()}
 
     @property
     def device_class(self):
@@ -176,18 +186,72 @@ class SensorMovingAvg(SensorEntity):
 class MovingAvg:
     """Moving average computation."""
 
-    def __init__(self, window: float, precision: int) -> None:
+    def __init__(self, name, window: timedelta, precision: int) -> None:
         """Initialize moving average."""
+        self._name = name
         self._window = window
         self._precision = precision
         self._data = deque()
 
-    def update_value(self, val: float, timestamp: float) -> float:
+    def update_value(self, val: float, timestamp: datetime) -> float | None:
         """Update moving average with value and timestamp."""
-        self._data.append([val, timestamp])
+        self._data.append(MovingAvg._tuple(val, timestamp))
         return self.update(timestamp)
 
-    def update(self, timestamp: float) -> float:
+    def update(self, timestamp: datetime) -> float | None:
         """Update moving average for timestamp."""
-        tuple = self._data.popleft()
-        return round(tuple[0], self._precision)
+        ret_val = None
+        size = len(self._data)
+        if size == 1:
+            ret_val = MovingAvg._value(self._data[0])
+        if size > 1:
+            # move window
+            start = timestamp - self._window
+            _LOGGER.debug(f"{self._name}: Window starts {start}")
+            removed = None
+            while MovingAvg._timestamp(self._data[0]) < start:
+                removed = self._data.popleft()
+                _LOGGER.debug(f"{self._name}: Removing {MovingAvg._value(removed)},{MovingAvg._timestamp(removed)}")
+            if ((removed is not None) and 
+               ((len(self._data) == 0) or (MovingAvg._timestamp(self._data[0]) > start))):
+                self._data.appendleft(MovingAvg._tuple(MovingAvg._value(removed), start))
+                _LOGGER.debug(f"{self._name}: Adding back {MovingAvg._value(removed)}")
+            # compute avg
+            if len(self._data) == 1:
+                ret_val = MovingAvg._value(self._data[0])
+            else:
+                duration = (timestamp - MovingAvg._timestamp(self._data[0])).total_seconds()
+                _LOGGER.debug(f"{self._name}: Duration is {duration}")
+                if duration > self._window.total_seconds():
+                    _LOGGER.error(f"{self._name}: Invalid duration - {duration} > {self._window.total_seconds()}")
+                ret_val = 0.0
+                prev = None
+                for cur in self._data:
+                    if prev is not None:
+                        weighted = MovingAvg._weighted(prev, MovingAvg._timestamp(cur), duration)
+                        _LOGGER.debug(f"{self._name}: Adding {weighted} - {MovingAvg._value(prev)},{MovingAvg._timestamp(prev)}, {MovingAvg._timestamp(cur)}")
+                        ret_val = ret_val + weighted
+                    prev = cur
+                if timestamp > MovingAvg._timestamp(cur):
+                    weighted = MovingAvg._weighted(cur, timestamp, duration)
+                    _LOGGER.debug(f"{self._name}: Adding {weighted} - {MovingAvg._value(cur)},{MovingAvg._timestamp(cur)}, {timestamp}")
+                    ret_val = ret_val + weighted
+        return (None if ret_val is None else round(ret_val, self._precision))
+
+    def data_points(self) -> int:
+        """Number of data points currently in window."""
+        return len(self._data)
+
+    def _timestamp(tuple: Tuple[float, datetime]) -> datetime:
+        return tuple[1]
+
+    def _value(tuple: Tuple[float, datetime]) -> float:
+        return tuple[0]
+
+    def _tuple(val: float, timestamp: datetime) -> Tuple[float, datetime]:
+        return [val, timestamp]
+
+    def _weighted(cur: Tuple[float, datetime], next: datetime, duration: float) -> float:
+        delta = (next - MovingAvg._timestamp(cur)).total_seconds()
+        return (MovingAvg._value(cur) * delta / duration)
+
